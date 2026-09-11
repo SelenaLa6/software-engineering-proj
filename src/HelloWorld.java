7f424f7c0f97e69ed0efdfb3cf1117246ec83d14
SelenaLa6 <selenala6@gmail.com>
2026-09-10 23:50:27 -0400

    chore: check that status checks fail

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main (String[] args) {
-        // This is an ok comment.
-        System.out.println("Hello World!");
+        //Comment
+        System.out.println("Hello World!")
     }
 }
